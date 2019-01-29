63e2124ebbb1745e6314d62ba7b6687a973bd961
avrezzon <anthonyrezz@gmail.com>
2019-01-28 17:47:10 -0700

    Added code from the slides provided

--- HW1/src/main/java/cs455/overlay/transport/TCPRecieverThread.java
+++ HW1/src/main/java/cs455/overlay/transport/TCPRecieverThread.java
@@ -1,5 +1,39 @@
 package main.java.cs455.overlay.transport;
 
-public class TCPRecieverThread {
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class TCPRecieverThread implements Runnable {
+
+  private Socket socket;
+  private DataInputStream din;
+
+  public TCPRecieverThread(Socket socket) throws IOException {
+    this.socket = socket;
+    din = new DataInputStream(socket.getInputStream());
+  }
+
+  public void run(){
+    int dataLength;
+    while(socket != null){
+      try{
+
+        dataLength = din.readInt();
+        byte[] data = new byte[dataLength];
+        din.readFully(data, 0, dataLength);
+
+      }catch (SocketException se){
+        System.err.println(se.getMessage());
+        break;
+      }catch (IOException ioe){
+        //Found an IOException
+        System.err.println(ioe.getMessage());
+        break;
+      }
+    }
+
+  }
 
 }
--- HW1/src/main/java/cs455/overlay/transport/TCPSender.java
+++ HW1/src/main/java/cs455/overlay/transport/TCPSender.java
@@ -1,5 +1,25 @@
 package main.java.cs455.overlay.transport;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.IO;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class TCPSender {
+
+  private Socket socket;
+  private DataOutputStream dout;
+
+  public TCPSender(Socket socket) throws IOException {
+    this.socket = socket;
+    dout = new DataOutputStream(socket.getOutputStream());
+  }
+
+  public void sendData(byte[] dataToSend) throws IOException {
+    int dataLength = dataToSend.length;
+    dout.writeInt(dataLength);
+    dout.write(dataToSend, 0 , dataLength);
+    dout.flush();
+  }
 
 }
