0f641d5b807b16a78ba13d9a106c1ac3ca6bf153
avrezzon <anthonyrezz@gmail.com>
2019-03-01 14:16:43 -0700

    This starts to setup the stuff with the client node link list

--- HW2/src/main/java/cs455/scaling/server/ThreadPoolManager.java
+++ HW2/src/main/java/cs455/scaling/server/ThreadPoolManager.java
@@ -1,12 +1,43 @@
 package cs455.scaling.server;
 
-//Determine what fields this class needs to have
-public class ThreadPoolManager {
-  //Needs to be able to manage all of the Worker Threads in the thread pool
-  //Remember the concurrentHashMap and the potential usages it could bring
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-  //TODO Collection of the worker threads
-  //TODO a queue of all of the tasks that have been received
-  //TODO ConcurrentHashMap for all the keys to the channels for the clients registered
+public class ThreadPoolManager {
+
+    private static ArrayList<WorkerThread> threadPool; //FIXME this could be a queue of threads
+    private static ConcurrentLinkedQueue<Task> taskQueue;
+    private static LinkedList<ClientNode> clients; //FIXME Im not sure if access is synchronized
+    private static ConcurrentHashMap<String, String> clientConnections; //TODO fix the key value pairing
+
+    private int maxBatchSize;
+    private int maxBatchTime;
+
+    //thread-pool-size batch-size batch-time
+    public ThreadPoolManager(int threadPoolSize, int maxBatchSize, int maxBatchTime){
+        for(int i = 0; i < threadPoolSize; i++){
+            threadPool.add(new WorkerThread());
+        }
+        clientConnections = new ConcurrentHashMap<>(16); //Not sure how many keys i should allow up to
+        this.maxBatchSize = maxBatchSize;
+        this.maxBatchTime = maxBatchTime;
+    }
+
+    private void bootup(){
+        for(WorkerThread worker : threadPool){
+            new Thread(worker).start();
+        }
+    }
+
+    public synchronized void register(String clientInfo){
+        clients.add(new ClientNode(this.maxBatchSize, this.maxBatchTime));
+    }
+
+    public synchronized static void addTask(Task task){
+        taskQueue.add(task);
+        //TODO need to notify the thread pool of a job
+    }
 
 }
--- HW2/src/main/java/cs455/scaling/wireformats/Payload.java
+++ HW2/src/main/java/cs455/scaling/wireformats/Payload.java
@@ -10,11 +10,14 @@ import java.security.SecureRandom;
 public class Payload {
 
   private byte[] msg;
+  private String hash;
 
+  //NOTE: hash is set to null to prevent a message from being passed without the
   //This will be used when creating the message from the client side
   public Payload() throws NoSuchAlgorithmException {
     msg = new byte[8000];
     SecureRandom.getInstanceStrong().nextBytes(msg);
+    hash = null;
   }
 
   //This will be the constructor that the server will use to make sure that the msg is consistant
@@ -22,10 +25,13 @@ public class Payload {
     this.msg = msg.clone();
   }
 
+  //The hash needs to be saved so that when the server does send a responsed
   public String calculateMsgHash() throws NoSuchAlgorithmException {
-    return Hash.SHA1FromBytes(msg);
+    this.hash = Hash.SHA1FromBytes(msg);
+    return this.hash;
   }
 
+  //FIXME this needs to include the string has so that this can be sent
   public byte[] getBytes() {
     return this.msg.clone();
   }
