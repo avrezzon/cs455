e0f70c48aab6867733c16e5248a2769ed73e2e07
avrezzon <anthonyrezz@gmail.com>
2019-03-07 00:07:05 -0700

    This is fixing the linked list of batches for the keys

--- HW2/src/main/java/cs455/scaling/server/ThreadPoolManager.java
+++ HW2/src/main/java/cs455/scaling/server/ThreadPoolManager.java
@@ -1,6 +1,7 @@
 package cs455.scaling.server;
 
 import cs455.scaling.protocol.Task;
+import java.nio.channels.SelectionKey;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -10,8 +11,15 @@ public class ThreadPoolManager {
   private ArrayList<WorkerThread> threadPool;
   public static ConcurrentLinkedQueue<Task> taskQueue;
   private static LinkedList<Batch> messageBatch;
-  private int maxBatchSize;
-  private int maxBatchTime;
+
+  //NOTE that this will not always be 0, this way we can append to other links
+  //This was marked as volatile so that every time it is fetched the current thread knows exactly what
+  //node to write to
+  //TODO this is a large factor that definitely needs to work
+  private static volatile int headNodeIdx;
+
+  private static int maxBatchSize;
+  private static int maxBatchTime;
 
   public ThreadPoolManager(int threadPoolSize, int maxBatchSize, int maxBatchTime) {
 
@@ -22,7 +30,9 @@ public class ThreadPoolManager {
     this.maxBatchSize = maxBatchSize;
     this.maxBatchTime = maxBatchTime;
     messageBatch = new LinkedList<Batch>();
-    taskQueue = new ConcurrentLinkedQueue<>(); //TODO verify this data structure
+    taskQueue = new ConcurrentLinkedQueue<>();
+
+    headNodeIdx = 0; //Upon initialization the head node should be the first node
   }
 
   //This is invoked once the server gets activity on a channel
@@ -38,6 +48,21 @@ public class ThreadPoolManager {
     for (int i = 0; i < threadPool.size(); i++) {
       WorkerThread worker = threadPool.get(i);
       new Thread(worker).start();
+    }
+  }
+
+  //With this method we will check the current head node to determine if the batch needs to be sent out back to
+  //The clients.  NOTE upon a successful detatchment, the method will insert a new link as the head node prior to the detachment
+  public synchronized static void addMsgKey(SelectionKey key) {
+
+    //We want to check to see if the head node is max batch size a new
+    if (messageBatch.get(headNodeIdx).dispatch()) {
+      //This is the case where the batch should be removed from the head of the linked list
+      //from the detachment, a new task should be added to the taskQueue with the dispatch batch object
+      //that the worker thread will perform work on
+
+    } else {
+
     }
   }
 
