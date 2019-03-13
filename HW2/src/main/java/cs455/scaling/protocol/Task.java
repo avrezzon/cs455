3ebea3fa27ea6aaed5bb0bca7c6f207d3ff87997
avrezzon <anthonyrezz@gmail.com>
2019-03-13 13:16:02 -0600

    cleaning up to test dispatching messages

--- HW2/src/main/java/cs455/scaling/protocol/Task.java
+++ HW2/src/main/java/cs455/scaling/protocol/Task.java
@@ -13,38 +13,24 @@ public class Task {
   //This is the task that is created for registering
   public Task(SelectionKey key) {
     this.key = key;
-    this.dispatch = false;
-  }
-
-  public Task(boolean dispatch) {
-    this.key = null;
-    this.dispatch = dispatch;
   }
 
   //This will do the function based upon the type of action so that the worker thread can just call the resolve fn
   public void resolve() {
     try {
+      //Need to validate that we aren't trying to read from an already closed channel
+      if (this.key.isValid()) {
 
-      if (dispatch) {
+        //An acceptable flag will show that we need to connect to a new client
+        if (this.key.isAcceptable()) {
+          Server.register(this.key);
+        }
 
-
-
-      } else {
-        //Need to validate that we aren't trying to read from an already closed channel
-        if (this.key.isValid()) {
-
-          //An acceptable flag will show that we need to connect to a new client
-          if (this.key.isAcceptable()) {
-            Server.register(this.key);
-          }
-
-          //This will extract the key from the task and pass it into the linked list of batches
-          if (this.key.isReadable()) {
-            ThreadPoolManager.addMsgKey(this.key);
-          }
+        //This will extract the key from the task and pass it into the linked list of batches
+        if (this.key.isReadable()) {
+          ThreadPoolManager.addMsgKey(this.key);
         }
       }
-
     } catch (IOException ie) {
       System.err.println(ie.getMessage());
     }
